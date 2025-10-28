71d093956fabacb0b072bb60a142333904557a90
prabhat1001 <prabhatsaraswat24@gmail.com>
2025-10-28 23:59:23 +0530

    add: new page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './index.css'
 import LandingPage from './pages/LandingPage/LandingPage.jsx'
+import WeProvide from './pages/WeProvide/WeProvide.jsx'
 
 const App = () => {
   return (
     <div >
       <LandingPage />
+      <WeProvide />
     </div>
   )
 }
