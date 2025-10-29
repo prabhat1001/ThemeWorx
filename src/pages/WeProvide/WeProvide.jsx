ed1e6c5c8759e429c1d7517124c0228123301547
prabhat1001 <prabhatsaraswat24@gmail.com>
2025-10-29 23:20:13 +0530

    made text responsive in cards

--- src/pages/WeProvide/WeProvide.jsx
+++ src/pages/WeProvide/WeProvide.jsx
@@ -8,19 +8,35 @@ const WeProvide = () => {
             <h1 className='text-5xl font-poppins font-bold mt-4'>We Provide</h1>
             <p className='mt-2 text-md font-poppins text-[#626262]'>Experience the magic of coding with customized themes <br/> that match your passions.</p>
         </div>
-        <div className='w-full mt-8 h-[120vh] border-2 border-amber-600 lg:flex-row md:flex-row gap-5 flex flex-col items-center'>
-            <div className='w-full lg:w-[65%] h-full border-2 bg-amber-200 flex flex-col gap-5'>
-                <div className='border-2 border-red-600 w-full h-[50%] gap-5 flex'>
-                    <div className='w-1/2 border-2 rounded-2xl '>card1</div>
-                    <div className='w-1/2 border-2 rounded-2xl'>card2</div>
+        <div className='w-full bg-amber-400 mt-8 md:h-[70vh] lg:h-[120vh]  lg:flex-row md:flex-row gap-5 flex flex-col items-center'>
+            <div className='w-full lg:w-[65%] h-full flex flex-col md:gap-2 lg:gap-5'>
+                <div className=' w-full h-[50%] gap-5 flex'>
+                    <div className='w-1/2 border-4 border-[#e6e5e5]  rounded-2xl font-poppins bg-[#f4f4f4] hover:bg-white md:p-3 lg:p-6 flex flex-col  lg:gap-4 hover:shadow-lg '>
+                        <h1 className='md:text-2xl lg:text-5xl font-medium text-[#6579C1]'>01</h1>
+                        <h2 className=' md:text-xl lg:text-3xl font-medium '>Immersive<br/>Experience</h2>
+                        <p className='text-[#393939] text-sm'>Dive into your favorite movies or anime through beautifully crafted themes that enhance your coding environment and creativity.</p>
+                    </div>
+                    <div className='w-1/2 border-4 border-[#e6e5e5] rounded-2xl font-poppins bg-[#f4f4f4] hover:bg-white md:p-3 lg:p-6 flex flex-col lg:gap-4 hover:shadow-lg'>
+                        <h1 className='md:text-2xl lg:text-5xl font-medium text-[#6579C1]'>02</h1>
+                        <h2 className='md:text-xl lg:text-3xl font-medium '>Tailored<br/>Environment</h2>
+                        <p className='text-[#393939] text-sm'>Customize your VSCode setup to match your personal style and mood, making coding enjoyable and inspiring every day.</p>
+                    </div>
                 </div>
-                <div className='border-2 border-red-600 w-full h-[50%] gap-5 flex'>
-                    <div className='w-1/2 border-2 rounded-2xl'>card3</div>
-                    <div className='w-1/2 border-2 rounded-2xl'>card4</div>
+                <div className=' w-full h-[50%] gap-5 flex'>
+                    <div className='w-1/2 border-4 border-[#e6e5e5]  rounded-2xl font-poppins bg-[#f4f4f4] hover:bg-white md:p-3 lg:p-6 flex flex-col lg:gap-4 hover:shadow-lg'>
+                        <h1 className='md:text-2xl lg:text-5xl font-medium text-[#6579C1]'>03</h1>
+                        <h2 className='md:text-xl lg:text-3xl font-medium '>Boost in<br/>Productivity</h2>
+                        <p className='text-[#393939] text-sm'>Stay motivated and focused by working in creative environments designed to energize and enhance your coding workflow.</p>
+                    </div>
+                    <div className='w-1/2 border-4 border-[#e6e5e5]  rounded-2xl font-poppins bg-[#f4f4f4] hover:bg-white p-6 flex flex-col gap-4 hover:shadow-lg'>
+                        <h1 className='text-5xl font-medium text-[#6579C1]'>04</h1>
+                        <h2 className='text-3xl font-medium '>Easy<br/>Integration</h2>
+                        <p className='text-[#393939]'>Switch seamlessly between various themes with hassle-free installation and smooth configuration, all in one place.</p>
+                    </div>
                 </div>
             </div>
-            <div className='w-full lg:w-[35%] h-full border-4 rounded-3xl'>
-                <img src={Skelly} alt="skelly gif" className='w-full h-full object-cover rounded-3xl p-2 ' />
+            <div className='w-full lg:w-[35%] h-full border-4 rounded-3xl  '>
+                <img src={Skelly} alt="skelly gif" className='w-full h-full p-2 object-cover rounded-3xl  ' />
             </div>
         </div>
     </div>
