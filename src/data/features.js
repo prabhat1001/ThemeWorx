6142201692fe1b26de220d45789e81f2c18cecc3
prabhat1001 <prabhatsaraswat24@gmail.com>
2025-10-30 23:22:27 +0530

    create data for cards

---
+++ src/data/features.js
@@ -0,0 +1,26 @@
+export const features = [
+  {
+    number: "01",
+    title: "Immersive Experience",
+    description:
+      "Dive into your favorite movies and anime through our beautifully crafted themes that will enhance your coding environment and creativity.",
+  },
+  {
+    number: "02",
+    title: "Tailored Environment",
+    description:
+      "Customize your VSCode setup to match your personal style and mood, making coding enjoyable and inspiring every day.",
+  },
+  {
+    number: "03",
+    title: "Boost in Productivity",
+    description:
+      "Stay motivated and focused by working in creative environments designed to energize and enhance your coding workflow.",
+  },
+  {
+    number: "04",
+    title: "Easy Integration",
+    description:
+      "Switch seamlessly between various themes with hassle-free installation and smooth configuration, all in one place.",
+  },
+];
