204d2d72483ee78fe9338d6d0c7e4d779da1bdca
prabhat1001 <prabhatsaraswat24@gmail.com>
2025-10-31 23:53:36 +0530

    explore themes page added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './index.css'
 import LandingPage from './pages/LandingPage/LandingPage.jsx'
 import WeProvide from './pages/WeProvide/WeProvide.jsx'
+import ExploreThemes from './pages/ExploreThemes/ExploreThemes.jsx'
 
 const App = () => {
   return (
     <div >
       <LandingPage />
       <WeProvide />
+      <ExploreThemes />
     </div>
   )
 }
