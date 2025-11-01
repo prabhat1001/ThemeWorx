1a96c777a4c06a1534fec600c3a97d3c4eb34f3d
prabhat1001 <prabhatsaraswat24@gmail.com>
2025-11-01 22:52:52 +0530

    path correction done

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import background from "../assets/forest-bg.jpeg";
+import CentreImg from "../assets/mystical.jpeg";
+import Navbar from "../components/Navbar";
+// import LogoImg from '../../assets/logo.png';
+
+
+
+const LandingPage = () => {
+  return (
+    <div className="max-w-[100vw] relative overflow-hidden ">
+
+      {/* Landing 1 */}
+      <div className=" w-[100vw] lg:h-[130vh] h-screen">
+
+        <div className="absolute z-20 w-full"><Navbar /></div>
+
+        {/* Hero title Section */}
+        <div>
+          <div className="  absolute z-10 mt-[30vh] w-full flex flex-col items-center px-4">
+            <h1 className="text-white font-poppins font-semibold lg:text-7xl md:text-5xl text-5xl text-center leading-tight drop-shadow-lg"> Craft Your <br /> Coding <span className="text-[#a6fa7f] font-semibold">Realm.</span></h1>
+            <p className="text-white font-poppins lg:text-lg md:text-md text-sm text-center mt-4 max-w-2xl drop-shadow-md">
+              Bring your favorite worlds to life with themes inspired by<br/>
+              movies, anime, and games. <br/><span className="text-[#a6fa7f] font-semibold"> Code your way!</span></p>
+          </div>
+        </div>
+
+        {/* background blur */}
+        <div
+          className="
+        z-5
+        w-full lg:h-[130vh] h-screen
+        absolute left-0 top-0  
+        brightness-80 xl:backdrop-blur-[4px] md:backdrop-blur-[3px] backdrop-blur-[3px]"
+        ></div>
+
+        {/* background image */}
+        <img src={background} className="w-full h-full object-cover" />
+      </div>
+
+      {/* Middle Img */}
+      <div className=" absolute left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 lg:w-[70vw] md:w-[70vw] w-[90vw] aspect-video  rounded-3xl shadow-xl/20 ">
+        <img
+          src={CentreImg}
+          className="w-full h-full object-cover rounded-3xl contrast-125 brightness-75 "
+        />
+      </div>
+
+      {/* Landing 2 */}
+      <div className=" w-[100vw] h-[80vh] lg:h-[110vh]  ">
+    
+    {/* Why? heading */}
+    <div className="  absolute z-20 mt-[30vh] lg:mt-[60vh] w-full flex flex-col items-center px-4 ">
+      <div className=" relative  uppercase text-8xl md:text-9xl lg:text-9xl font-inter font-bold 
+      bg-gradient-to-b from-black from-30% to-transparent to-80% bg-clip-text text-transparent">
+        WHY?
+      </div>
+      <div className=" font-LaBelleAurore lg:text-4xl md:text-4xl text-3xl text-center">
+        " Because it's Cool "
+      </div>
+      <div className=" mt-6 font-poppins text-md text-center max-w-3xl text-[#929292]">
+        Turn your coding hours into an adventure. <br/> Escape the boring. Build magic.
+      </div>
+    </div>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
---
+++ src/pages/WeProvide.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { features } from "../data/features";
+import FeatureCard from "../components/FeatureCard";
+import Skelly from "../assets/skelly.gif";
+
+const WeProvide = () => {
+  return (
+    <section className="w-full min-h-screen px-6 md:px-10 py-16 bg-white font-poppins">
+      {/* Header */}
+      <div className=" mb-12">
+        <h1 className="text-4xl md:text-5xl font-bold text-[#1f1f1f]">
+          We Provide
+        </h1>
+        <p className="mt-3 text-md text-[#626262]">
+          Experience the magic of coding with customized themes <br />
+          that match your passions.
+        </p>
+      </div>
+
+      {/* Content Grid */}
+      <div className="flex flex-col lg:flex-row items-center gap-10 lg:gap-6 ">
+        {/* Feature Cards */}
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full lg:w-2/3">
+          {features.map((feature) => (
+            <FeatureCard key={feature.number} {...feature} />
+          ))}
+        </div>
+
+        {/* Image Section */}
+        <div className="w-full lg:h-full md:h-[90vh] lg:w-1/3 rounded-3xl overflow-hidden border-4 border-[#e6e5e5]">
+          <img
+            src={Skelly}
+            alt="Animated skeleton coding on computer"
+            className="w-full h-full object-cover rounded-4xl p-4 "
+            loading="lazy"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default WeProvide;
