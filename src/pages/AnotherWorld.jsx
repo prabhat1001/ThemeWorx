0b61eac40d257a67506933d246bad9c7f175b445
prabhat1001 <prabhatsaraswat24@gmail.com>
2025-11-03 01:07:31 +0530

    Another world glimpse for them style

--- src/pages/AnotherWorld.jsx
+++ src/pages/AnotherWorld.jsx
@@ -3,7 +3,7 @@ import AnotherWorldImg from '../assets/space.jpg'
 
 const AnotherWorld = () => {
   return (
-    <div className='w-full h-[80vh] bg-green-200'>
+    <div className='mt-8 w-full h-screen lg:h-[60vh] bg-green-200'>
         <img src={AnotherWorldImg} alt="" className='w-full h-full object-cover'/>
     </div>
   )
