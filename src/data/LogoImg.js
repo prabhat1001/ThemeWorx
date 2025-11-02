7a771702f05d77c994c766af56b7ab93faeebb30
prabhat1001 <prabhatsaraswat24@gmail.com>
2025-11-03 01:06:29 +0530

    data for logo carousel added

---
+++ src/data/LogoImg.js
@@ -0,0 +1,34 @@
+import HarryPotter from '../assets/HarryPotter.png';
+import StarWars from '../assets/StarWars.png';
+import Ghibili from '../assets/Ghibili.png';
+import StrangerThings from '../assets/StrangerThings.png';
+import JurassicPark from '../assets/JurassicPark.png';
+
+export const LogoImg = [
+    {
+        id: 1,
+        src: HarryPotter,
+        alt: 'Harry Potter Logo',
+    },
+    {
+        id: 2,
+        src: StarWars,
+        alt: 'Star Wars Logo',
+    },
+    {
+        id: 3,
+        src: Ghibili,
+        alt: 'Ghibili Logo',
+    },
+    {
+        id: 4,
+        src: StrangerThings,
+        alt: 'Stranger Things Logo',
+    },
+    {
+        id: 5,
+        src: JurassicPark,
+        alt: 'Jurassic Park Logo',
+    },
+
+];
