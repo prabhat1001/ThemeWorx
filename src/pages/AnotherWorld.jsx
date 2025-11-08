5be729b8db9da2dca0697f988d1e32dbcdf84356
prabhat1001 <prabhatsaraswat24@gmail.com>
2025-11-08 17:39:43 +0530

    section divider using an asset in b/w

--- src/pages/AnotherWorld.jsx
+++ src/pages/AnotherWorld.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import AnotherWorldImg from '../assets/space.jpg'
+// import AnotherWorldImg from '../assets/space.jpg'
+import Fox from '../assets/fox.png'
 
 const AnotherWorld = () => {
   return (
     <div className='mt-8 w-full h-screen lg:h-[60vh] bg-green-200'>
-        <img src={AnotherWorldImg} alt="" className='w-full h-full object-cover'/>
+        <img src={Fox} alt="" className='w-full h-full object-cover'/>
     </div>
   )
 }
