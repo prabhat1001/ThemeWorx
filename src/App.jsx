f451d46e575dfd814787f52d4e60527a5c93e6cf
prabhat1001 <prabhatsaraswat24@gmail.com>
2025-12-02 14:03:57 +0530

    Themes Detail page added along with  auto ScrollToTop

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,22 @@ import { Routes, Route, useLocation } from "react-router-dom";
 import './index.css'
 import HomePage from './pages/HomePage'
 import ThemesWorld from './pages/ThemesWorld'
+import ThemesDetail from './pages/ThemesDetail'
+import ScrollToTop from './components/ScrollToTop';
 
 const App = () => {
   const location = useLocation();
 
   return (
+    <>
+      <ScrollToTop /> 
+      
       <Routes location={location} key={location.pathname}>
       <Route path='/' element={<HomePage />} />
       <Route path='/themes' element={<ThemesWorld />} />
+      <Route path="/themes/:slug" element={<ThemesDetail />} />
       </Routes>
+    </>
   )
 }
 
