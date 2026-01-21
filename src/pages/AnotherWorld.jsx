8dc352d1695800cfc61de4e4f52253fd12cfd221
prabhat1001 <prabhatsaraswat24@gmail.com>
2026-01-22 00:01:18 +0530

    update: new image changes instead of medic fox

--- src/pages/AnotherWorld.jsx
+++ src/pages/AnotherWorld.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 // import AnotherWorldImg from '../assets/space.jpg'
-import Fox from '../assets/fox.png'
+// import Fox from '../assets/fox.png'
+import PixelWorld from '../assets/pixelworld.jpg'
 
 const AnotherWorld = () => {
   return (
     <div className='mt-8 w-full h-screen lg:h-[60vh] bg-green-200'>
-        <img src={Fox} alt="" className='w-full h-full object-cover'/>
+        <img src={PixelWorld} alt="" className='w-full h-full object-cover'/>
     </div>
   )
 }
