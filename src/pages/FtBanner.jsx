1c6a6ada5ed7f6f237fb8688055fa833595d6d28
prabhat1001 <prabhatsaraswat24@gmail.com>
2026-02-01 00:29:42 +0530

    update: removed border

--- src/pages/FtBanner.jsx
+++ src/pages/FtBanner.jsx
@@ -5,7 +5,7 @@ import SpaceShip from '../assets/space-shuttle.jpg'
 
 const FtBanner = () => {
   return (
-    <div className='w-full h-screen lg:h-[60vh] border-3 border-green-800 '>
+    <div className='w-full h-screen lg:h-[60vh]'>
         <img src={SpaceShip} alt="Ground" className='w-full h-full object-cover' />
         <div className='w-full h-[3rem] bg-black bottom-vignette z-20 relative'></div>
     </div>
