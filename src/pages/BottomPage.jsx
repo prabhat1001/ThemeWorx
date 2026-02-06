04f48bed518e9cbe98ca8b44377618975c536b63
prabhat1001 <prabhatsaraswat24@gmail.com>
2026-02-07 00:41:26 +0530

    fix: styling in bottomPage

--- src/pages/BottomPage.jsx
+++ src/pages/BottomPage.jsx
@@ -7,10 +7,10 @@ import Jedi from '../assets/jedi.jpeg'
 
 const BottomPage = () => {
   return (
-    <div className="max-w-screen relative overflow-hidden ">
+    <div className="max-w-screen relative overflow-hidden">
 
         {/* Bottom 1 */}
-        <div className=" w-[100vw] lg:h-[80vh] md:h-[60vh] h-[50vh] flex flex-col justify-center items-center gap-10  mb-[20vh] md:mb-[20vh] lg:mb-[30vh]">
+        <div className=" w-[100vw] lg:h-[80vh] md:h-[60vh] h-[50vh] flex flex-col justify-center items-center gap-10  mb-[10vh]">
             <p className='w-full h-[10vh] flex justify-center items-center font-LaBelleAurore text-[#383838] text-2xl md:text-3xl lg:text-2xl '>Themes Inspired From:</p>
             <InfiniteLogo />
         </div>
