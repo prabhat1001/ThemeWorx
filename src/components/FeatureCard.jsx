1a9eaddab8715c2448218810ffe067e8fc5c5a5e
prabhat1001 <prabhatsaraswat24@gmail.com>
2026-02-12 23:26:17 +0530

    update: feature cards UI updated

--- src/components/FeatureCard.jsx
+++ src/components/FeatureCard.jsx
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 
 const FeatureCard = ({ number, title, description }) => {
   return (
-    <div className="card border-2 border-[#e6e5e5] rounded-2xl bg-[#f4f4f4] hover:bg-white p-6 flex flex-col gap-3 hover:shadow-lg transition-transform duration-300 hover:-translate-y-2">
+    <div className="card border-2 border-[#e6e5e5] rounded-2xl bg-[#f4f4f4] hover:bg-white p-6 flex flex-col gap-3 justify-center hover:shadow-lg transition-transform duration-300 hover:-translate-y-2">
       <h1 className="text-5xl font-semibold text-[#6579C1]">{number}</h1>
       <h2 className="text-2xl font-semibold text-[#1f1f1f] w-[8rem]">{title}</h2>
       <p className="text-[#393939] text-sm leading-relaxed mb-3 ">{description}</p>
-      <button className="mt-auto self-start px-4 py-2 text-[#6579C1] border-2 border-[#5568a8] rounded-[2rem] hover:bg-[#5568a8] hover:text-white transition-colors duration-300">
+      {/* <button className="mt-auto self-start px-4 py-2 text-[#6579C1] border-2 border-[#5568a8] rounded-[2rem] hover:bg-[#5568a8] hover:text-white transition-colors duration-300">
         Learn More
-        </button>
+        </button> */}
     </div>
   );
 };
