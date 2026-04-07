8336d83dd5a41c7fd1bf251610d306f6d43b94f8
prabhat1001 <prabhatsaraswat24@gmail.com>
2026-04-07 23:41:10 +0530

    Fix: using syntax highlighter only for json and javascript instead of all kinds for languages

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import json from "react-syntax-highlighter/dist/esm/languages/prism/json";
 import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import { FiCopy, FiCheck } from "react-icons/fi";
+
+SyntaxHighlighter.registerLanguage("json", json);
 
 const CodeBlock = ({ code }) => {
   const [copied, setCopied] = useState(false);
