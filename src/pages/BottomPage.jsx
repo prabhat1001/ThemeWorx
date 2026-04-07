a8219746a223cf777f27c2eea41b32fb0985e84d
prabhat1001 <prabhatsaraswat24@gmail.com>
2026-04-07 23:44:42 +0530

    removed: unused libraries, assets and unnecessary code

--- src/pages/BottomPage.jsx
+++ src/pages/BottomPage.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { LogoImg } from '../data/LogoImg.js'
 import InfiniteLogo from '../components/InfiniteLogo.jsx'
-import WarningMarquee  from '../components/WarningMarquee.jsx'
-import Jedi from '../assets/jedi.jpeg'
+
 
 
 const BottomPage = () => {
@@ -14,33 +12,6 @@ const BottomPage = () => {
             <p className='w-full h-[10vh] flex justify-center items-center font-LaBelleAurore text-[#383838] text-2xl md:text-3xl lg:text-2xl '>Themes Inspired From:</p>
             <InfiniteLogo />
         </div>
-
-
-        {/* CTA */}
-              {/* <div className=" absolute left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 lg:h-[25vw] md:h-[35vw] aspect-video w-[90vw]  rounded-3xl shadow-xl/30 flex ">
-                <img
-                  src={Jedi}
-                  alt="Jedi themed coding banner"
-                  className="w-full h-full object-cover rounded-3xl contrast-125 brightness-75 "
-                />
-                <div className='absolute flex flex-col items-end pr-[5vw] justify-center w-full h-full bg-gradient-to-l from-black from-10% to-transparent to-90% rounded-3xl'>
-                  <h1 className='lg:text-5xl md:text-3xl text-white font-poppins font-semibold'>Code without limit <span className='text-green-600'>.</span></h1>
-                  <p className='text-lg md:text-[1rem] w-[55%] text-[#cccccc] font-poppins text-right'>find the right theme for your project, easy apply in minutes.</p>
-                </div>
-              </div> */}
-        
-            
-        {/* Bottom 2 */}
-        {/* <div className=" relative w-[100vw] lg:h-[120vh] md:h-[70vh] h-[70vh] bg-[radial-gradient(circle_at_center,#001820,#000000)] flex flex-col justify-center items-center ">
-
-        
-          <div className=' z-11 lg:-rotate-8 md:-rotate-15 -rotate-20 mt-[15vh]'>
-            <WarningMarquee direct={'right'} />
-          </div>
-          <div className=' z-10 rotate-12 mt-6'>
-            <WarningMarquee direct={'left'}  />
-          </div>
-        </div> */}
     </div>
   )
 }
