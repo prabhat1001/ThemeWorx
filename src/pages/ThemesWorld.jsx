8e07a8b6db7d19c0ae0cf3061af21b139beb2222
prabhat1001 <prabhatsaraswat24@gmail.com>
2026-05-13 11:59:10 +0530

    style: adjusted responsiveness

--- src/pages/ThemesWorld.jsx
+++ src/pages/ThemesWorld.jsx
@@ -12,21 +12,29 @@ const ThemesWorld = () => {
           <div className=' relative border-2 lg:w-[94vw] md:w-[90vw] w-[90vw] h-[90vh] mt-8 rounded-tl-[2rem] rounded-tr-[2rem] flex flex-col overflow-hidden'>
             <img src={Castle} alt="dark castle" className='w-full h-full object-cover' />
             <div className=' absolute bottom-0 w-full h-[40vh]  bg-gradient-to-t from-black from-30% to-transparent to-80%'></div>
-            <h1 className='absolute leading-tight bottom-0 right-0 pb-10 pr-10 text-right font-poppins lg:text-6xl md:text-7xl text-4xl text-white font-semibold'>Explore&nbsp; <br/> Your World<span className='text-[#2AAB64]'>.</span></h1>
+            <h1 className='absolute leading-tight bottom-0 right-0 pb-10 pr-10 text-right font-poppins lg:text-6xl min-[500px]:text-6xl text-4xl text-white font-semibold'>Explore&nbsp; <br/> Your World<span className='text-[#2AAB64]'>.</span></h1>
           </div>
       </div>
 
-      {/* card grid in grid of 3 cards with big square image, dynamically responsive */}
+      {/* card grid in grid of 3 cards with big square image, dynamically responsive  */}
       
-        <div className=" w-full min-h-screen flex justify-center items-center p-10 ">
-          <div className=' grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 w-full max-w-[1200px]'>
-            {ThemesData.map((card) => (
-              <Link to={`${card.slug}`} key={card.id} className=" w-full lg:h-[60vh] md:h-[30vh] h-[50vh]  rounded-[2rem] overflow-hidden shadow-lg">
-                <img src={card.src} alt={card.alt} className=' w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out'/>
-              </Link>
-            ))}
-          </div>
-        </div>
+            <div className="w-full min-h-screen flex justify-center items-center p-10">
+              <div className='grid grid-cols-1 min-[500px]:grid-cols-2 lg:grid-cols-3 gap-10 min-[500px]:gap-5 w-full max-w-[1200px]'>
+                {ThemesData.map((card) => (
+                  <Link 
+                    to={`${card.slug}`} 
+                    key={card.id} 
+                    className="w-full aspect-square rounded-[2rem] min-[500px]:rounded-3xl overflow-hidden shadow-lg"
+                  >
+                    <img 
+                      src={card.src} 
+                      alt={card.alt} 
+                      className='w-full h-full object-cover hover:scale-105 transition-transform duration-300 ease-in-out'
+                    />
+                  </Link>
+                ))}
+              </div>
+            </div>
 
           {/* bottom Line */}
           <div className=' w-full h-[40vh] mb-[10vh]'>
